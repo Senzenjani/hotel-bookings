073447e4b2924ced36282c31e257bc14e615df1d
Senzenjani <smanjawira@gmail.com>
2023-11-23 09:51:00 +0200

    Added validator

---
+++ js/services/validator.js
@@ -0,0 +1,9 @@
+export function isEmptyObject(obj) {
+    var name;
+    for (name in obj) {
+        if (obj.hasOwnProperty(name)) {
+            return false;
+        }
+    }
+    return true;
+}
